28d19106c58c3eb118798517657865fae1315e6c
agiao <wyd_text@163.com>
2022-08-30 21:59:54 +0800

    Initial commit

---
+++ src/main/java/com/wyd/vbloglearn/VBlogLearnApplication.java
@@ -0,0 +1,13 @@
+package com.wyd.vbloglearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VBlogLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VBlogLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wyd/vbloglearn/VBlogLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyd.vbloglearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VBlogLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
